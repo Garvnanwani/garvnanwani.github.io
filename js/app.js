ff079ac3b37c58961c456a847d8310ea9b6cf667
Garv nanwani <garvnanwani16@gmail.com>
2020-09-16 19:59:29 +0530

    made small changes in each section

--- js/app.js
+++ js/app.js
@@ -3,14 +3,7 @@ const navBtn = document.querySelector("#nav-btn");
 const closeBtn = document.querySelector("#close-btn");
 const sidebar = document.querySelector("#sidebar");
 const date = document.querySelector("#date");
-// add fixed class to navbar
-window.addEventListener("scroll", function () {
-    if (window.pageYOffset > 80) {
-        navbar.classList.add("navbar-fixed");
-    } else {
-        navbar.classList.remove("navbar-fixed");
-    }
-});
+
 // show sidebar
 navBtn.addEventListener("click", function () {
     sidebar.classList.add("show-sidebar");
@@ -18,6 +11,7 @@ navBtn.addEventListener("click", function () {
 closeBtn.addEventListener("click", function () {
     sidebar.classList.remove("show-sidebar");
 });
+
 // set year
 date.innerHTML = new Date().getFullYear();
 
