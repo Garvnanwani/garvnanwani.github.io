ec8bfc1ca021f1056e167f7c4da4fe077f4b917b
Garv nanwani <garvnanwani16@gmail.com>
2020-09-21 00:40:11 +0530

    updated about section

--- js/app.js
+++ js/app.js
@@ -3,13 +3,16 @@ const navBtn = document.querySelector("#nav-btn");
 const closeBtn = document.querySelector("#close-btn");
 const sidebar = document.querySelector("#sidebar");
 const date = document.querySelector("#date");
+const body = document.querySelector("body");
 
 // show sidebar
 navBtn.addEventListener("click", function () {
     sidebar.classList.add("show-sidebar");
+    body.overflow = "hidden";
 });
 closeBtn.addEventListener("click", function () {
     sidebar.classList.remove("show-sidebar");
+    body.overflow = "auto";
 });
 
 // set year
