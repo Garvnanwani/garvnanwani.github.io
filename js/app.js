1e75b3833cd44f31bb347148484ccb675dd67c9c
Garv nanwani <garvnanwani16@gmail.com>
2020-09-21 01:01:38 +0530

    fixed responsiveness issue in small screens

--- js/app.js
+++ js/app.js
@@ -3,16 +3,13 @@ const navBtn = document.querySelector("#nav-btn");
 const closeBtn = document.querySelector("#close-btn");
 const sidebar = document.querySelector("#sidebar");
 const date = document.querySelector("#date");
-const body = document.querySelector("body");
 
 // show sidebar
 navBtn.addEventListener("click", function () {
     sidebar.classList.add("show-sidebar");
-    body.overflow = "hidden";
 });
 closeBtn.addEventListener("click", function () {
     sidebar.classList.remove("show-sidebar");
-    body.overflow = "auto";
 });
 
 // set year
