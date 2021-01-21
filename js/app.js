e38274a820e8b974af3000a3556b3bc6f087b635
Garv nanwani <garvnanwani16@gmail.com>
2021-01-22 01:52:09 +0530

    added smooth scroll to href links

--- js/app.js
+++ js/app.js
@@ -1,17 +1,27 @@
-const navbar = document.querySelector("#nav");
-const navBtn = document.querySelector("#nav-btn");
-const closeBtn = document.querySelector("#close-btn");
-const sidebar = document.querySelector("#sidebar");
-const date = document.querySelector("#date");
+const navbar = document.querySelector('#nav')
+const navBtn = document.querySelector('#nav-btn')
+const closeBtn = document.querySelector('#close-btn')
+const sidebar = document.querySelector('#sidebar')
+const date = document.querySelector('#date')
+
+//smooth scroll to href links
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault()
+
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth',
+    })
+  })
+})
 
 // show sidebar
-navBtn.addEventListener("click", function () {
-    sidebar.classList.add("show-sidebar");
-});
-closeBtn.addEventListener("click", function () {
-    sidebar.classList.remove("show-sidebar");
-});
+navBtn.addEventListener('click', function () {
+  sidebar.classList.add('show-sidebar')
+})
+closeBtn.addEventListener('click', function () {
+  sidebar.classList.remove('show-sidebar')
+})
 
 // set year
-date.innerHTML = new Date().getFullYear();
-
+date.innerHTML = new Date().getFullYear()
