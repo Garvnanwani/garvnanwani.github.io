313e780c9d1816f30a91682da819c5e0a998ecaa
Garv <garvwashere@gmail.com>
2021-05-24 14:29:43 +0530

    prettier and eslint setup

---
+++ next.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  future: {
+    webpack5: true,
+    strictPostcssConfiguration: true
+  },
+  reactStrictMode: true,
+  experimental: {
+    turboMode: true,
+    eslint: true
+  }
+};
