f90ff35f9cb21934194b05d800dc93ee8ca7c2fd
Garv <garvwashere@gmail.com>
2021-05-25 19:41:11 +0530

    created custom 404 page

---
+++ pages/404.js
@@ -0,0 +1,13 @@
+import Container from '@/components/Container'
+
+export default function PageNotFound() {
+  return (
+    <>
+      <Container>
+        <div className="w-full mx-auto mt-10 md:my-20 md:max-w-5xl">
+          You just want to go to a page that does not exist....
+        </div>
+      </Container>
+    </>
+  )
+}
