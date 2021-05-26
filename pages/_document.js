51c9269da254d7489eb2e4826f3d445c19d4532c
Garv <garvwashere@gmail.com>
2021-05-26 08:51:50 +0530

    added site analytics

---
+++ pages/_document.js
@@ -0,0 +1,30 @@
+import Document, { Head, Html, Main, NextScript } from 'next/document'
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <link
+            rel="preload"
+            href="/fonts/inter-var-latin.woff2"
+            as="font"
+            type="font/woff2"
+            crossOrigin="anonymous"
+          />
+          <link href="/static/favicons/favicon.ico" rel="shortcut icon" />
+
+          <meta content="#ffffff" name="theme-color" />
+          <meta content="#ffffff" name="msapplication-TileColor" />
+          <script async src="https://cdn.splitbee.io/sb.js"></script>
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default MyDocument
