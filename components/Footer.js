c97a7c171058618c2b952950c65d7ea13b16474b
Garv <garvwashere@gmail.com>
2021-05-26 11:03:08 +0530

    added spotify now playing feature

--- components/Footer.js
+++ components/Footer.js
@@ -1,4 +1,5 @@
 import Link from 'next/link'
+import NowPlaying from './NowPlaying'
 
 const ExternalLink = ({ href, children }) => (
   <a
@@ -14,6 +15,7 @@ const ExternalLink = ({ href, children }) => (
 export default function Footer() {
   return (
     <footer className="flex flex-col items-start justify-center w-full max-w-6xl mx-auto">
+      <NowPlaying />
       <hr className="w-full mb-8 border-gray-400 border-1 dark:border-gray-800" />
       <div className="grid w-full max-w-6xl grid-cols-3 gap-4 pb-16 mx-auto text-center">
         <div className="flex flex-col space-y-4">
