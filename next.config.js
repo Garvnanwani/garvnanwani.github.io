ea4e5aafdef251425681442ebd0349c205548513
Garv <garvwashere@gmail.com>
2021-06-01 14:21:47 +0530

    moved to preact in production

--- next.config.js
+++ next.config.js
@@ -17,5 +17,17 @@ module.exports = {
   experimental: {
     turboMode: true,
     eslint: true
+  },
+  webpack: (config, { dev, isServer }) => {
+    // Replace React with Preact only in client production build
+    if (!dev && !isServer) {
+      Object.assign(config.resolve.alias, {
+        react: 'preact/compat',
+        'react-dom/test-utils': 'preact/test-utils',
+        'react-dom': 'preact/compat'
+      })
+    }
+
+    return config
   }
 }
